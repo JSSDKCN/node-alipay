434373f87fd56687b3de6d7a4cc24789baab4dd2
calidion <calidion@gmail.com>
2014-12-06 02:57:44 +0800

    enabled mobile create

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var alipay = require('./lib/alipay');
+var config = require('./lib/config');
 var mobile = require('./lib/mobile');
 
+alipay.config = config;
 alipay.mobile = mobile;
 module.exports = alipay;
 
